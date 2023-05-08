4b609be6167103361465d693687a7ab74f2af6ed
dinhthien2000 <thiencamau1804@gmail.com>
2023-05-09 00:15:10 +0700

    first commit

---
+++ DemoMultiThreadV2/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    private static async Task Main(string[] args)
+    {
+        int a = 1;
+        Int32 b = 2;
+
+        string s = "1";
+
+        Console.WriteLine(int.Parse(s));
+        
+    }
+} 
